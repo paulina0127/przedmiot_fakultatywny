3119140047db6822a4b4a65eb9f9d25f87402275
DominikaJ <92153602+D0minikaJ@users.noreply.github.com>
2023-05-08 23:23:27 +0200

    Add HomeScreen && Add global styles
    What's new:
    Partially finished home screen (without router yet).

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <HomeScreen />
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
